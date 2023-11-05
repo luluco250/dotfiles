69825cc66384f56aebaf9c3c9be75649cc0f20b4
Lucas Melo <luluco250@gmail.com>
2023-11-05 18:42:18 -0300

    Changes

--- .config/fish/functions/wine32.fish
+++
@@ -1,4 +0,0 @@
-# Defined in - @ line 1
-function wine32 --wraps='env WINEPREFI="$HOME/.wine32" WINEARCH=win32 wine' --wraps='env WINEPREFIX="$HOME/.wine32" WINEARCH=win32 wine' --description 'alias wine32 env WINEPREFIX="$HOME/.wine32" WINEARCH=win32 wine'
-  env WINEPREFIX="$HOME/.wine32" WINEARCH=win32 wine $argv;
-end
